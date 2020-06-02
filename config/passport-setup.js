fd075d19a7d920f725bcbfe86d32fe61fa1e7b3d
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-02 20:26:50 +0530

    Added credentials for using GoogleStrategy

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,7 +2,8 @@ const passport=require('passport');
 const GoogleStrategy=require('passport-google-oauth20');
 
 passport.use(new GoogleStrategy({
-
+    clientID:'274057902758-2ubp2bk3u8dgfqm2fjja0hbtkqa3c0nh.apps.googleusercontent.com',
+    clientSecret:'QIR0mdnEP1maHx54kqMLnqvn'
 }),()=>{
-    
+
 })
