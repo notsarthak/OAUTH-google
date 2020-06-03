e57b8acf2cd721c3776ec508089d74f849290811
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-04 03:19:58 +0530

    Accessing user in redirect route and sending to the browser

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -21,7 +21,7 @@ router.get('/google',passport.authenticate('google',{
 
 //callback route for google to redirect
 router.get('/google/redirect',passport.authenticate('google'),(req,res)=>{
-    res.send('You have reached the redirect URI');
+    res.send(req.user);
 })
 
 module.exports=router;
