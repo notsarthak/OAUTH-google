dd95cbe440bbf4d448343f5bcef184e6e39fa8b9
Sarthak Gupta <guptasarthakdelhi2000@gmail.com>
2020-06-03 21:54:45 +0530

    Saving user to the database when their profile is recieved

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport=require('passport');
 const GoogleStrategy=require('passport-google-oauth20');
 const keys=require('./keys')
+const User=require('../models/user-model');
 //const dotenv=require('dotenv');
 
 /*dotenv.config({
@@ -22,4 +23,11 @@ passport.use(new GoogleStrategy({
 },(accessToken,refreshToken,profile,done)=>{
     console.log('passport callback function fired')
     console.log(profile); 
+    const user=new User({
+        googleId:profile.id,
+        username:profile.displayName
+    })
+    user.save().then((newUser)=>{
+        console.log('new user saved: '+newUser);
+    })
 }))
